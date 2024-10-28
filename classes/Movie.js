06beaeb505e44467b7fb268383874f63559f04e8
John Arocha <john.arocha@clover.com>
2024-10-28 11:10:00 -0600

    classes added, some test broken

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,26 @@
 // import the Media class:
+let Media = require('./Media')
 
 // create your Movie class:
+
+class Movie extends Media {
+    constructor(director, rating, summary, duration, title, year, genre, ) {
+        super(title, year, genre)
+        this.director = director
+        this.rating = rating
+        this.summary = summary
+        this.duration = duration
+    }
+    summary() {
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+    static longestMovie(movies) {
+        return movies.reduce((longest, current) => {
+            return (current.duration > longest.duration) ? current : longest;
+        });
+    }
+
+}
 
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,25 @@
 // import the Media class:
+let Media = require('./Media')
 
 // create your Music class:
+
+class Music extends Media {
+    constructor(artist, length, year, genre, title) {
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    // Method to return a summary of the music
+    summary() {
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+    }
+    static shortestAlbum(albums) {
+        return albums.reduce((shortest, album) => {
+            return album.length < shortest.length ? album : shortest;
+        });
+    }
+}
 
 // don't change below
 module.exports = Music;
